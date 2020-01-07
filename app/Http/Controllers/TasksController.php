726758b412fa1bd00c51ffa18d428f199014a833
kim whee <kimwhee8358@gmail.com>
2020-01-07 15:03:15 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -32,6 +32,7 @@ class TasksController extends Controller
     {
         $task =new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
@@ -61,6 +62,7 @@ class TasksController extends Controller
     {
         $task= Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
